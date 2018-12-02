ca9bb05ea8e388d5c7b843577555ccff3c09d199
Martin Tillmann <martin.tillmann@kit.edu>
2018-12-02 21:52:10 +0100

    don't use float in the JSON dump

--- tests/20-3cars_1lane.json.sol
+++ tests/20-3cars_1lane.json.sol
@@ -1,1 +1,1 @@
-{"cars":[{"from":0,"id":0,"lane":0,"position":383.1747741699219,"to":1},{"from":0,"id":1,"lane":0,"position":423.7786560058594,"to":1},{"from":0,"id":2,"lane":0,"position":471.2278137207031,"to":1}]}
+{"cars":[{"from":0,"id":0,"lane":0,"position":383.17477919465665,"to":1},{"from":0,"id":1,"lane":0,"position":423.77866322096435,"to":1},{"from":0,"id":2,"lane":0,"position":471.2278282794732,"to":1}]}
--- tests/25-2two_cars_before_lane_change.json.sol
+++ tests/25-2two_cars_before_lane_change.json.sol
@@ -1,1 +1,1 @@
-{"cars":[{"from":0,"id":0,"lane":1,"position":39.19553756713867,"to":1},{"from":0,"id":1,"lane":0,"position":79.60626220703125,"to":1}]}
+{"cars":[{"from":0,"id":0,"lane":1,"position":39.19553878963053,"to":1},{"from":0,"id":1,"lane":0,"position":79.60626228551754,"to":1}]}
