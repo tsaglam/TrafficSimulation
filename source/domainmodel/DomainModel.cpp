331a1d8ca9a5a6998525701aa2d33828f3e41295
Timur Saglam <uleay@student.kit.edu>
2018-12-19 16:40:04 +0100

    ADD: Added pseudo iterators to the domain model file. Note that they currently access the vectors of the DomainModel class through direct field access. This might need to be replaced by indirection through accessors.

--- source/domainmodel/DomainModel.cpp
+++ source/domainmodel/DomainModel.cpp
@@ -18,14 +18,74 @@ Junction& DomainModel::addJunction(Junction& junction) {
   return junctions.back();
 }
 
-Vehicle& DomainModel::getVehicle(id_type id) {
-	return vehicles.at(id);
+Vehicle& DomainModel::getVehicle(id_type id) { return vehicles.at(id); }
+
+Street& DomainModel::getStreet(id_type id) { return streets.at(id); }
+
+Junction& DomainModel::getJunction(id_type id) { return junctions.at(id); }
+
+/*
+ * class VehicleIterator:
+ */
+VehicleIterator::VehicleIterator(DomainModel& domainModel)
+    : domainModel(domainModel) {}
+
+std::vector<Vehicle>::const_iterator VehicleIterator::cbegin() {
+  return domainModel.vehicles.cbegin();
 }
 
-Street& DomainModel::getStreet(id_type id) {
-	return streets.at(id);
+std::vector<Vehicle>::const_iterator VehicleIterator::cend() {
+  return domainModel.vehicles.cend();
 }
 
-Junction& DomainModel::getJunction(id_type id) {
-	return junctions.at(id);
+std::vector<Vehicle>::iterator VehicleIterator::begin() {
+  return domainModel.vehicles.begin();
+}
+
+std::vector<Vehicle>::iterator VehicleIterator::end() {
+  return domainModel.vehicles.end();
+}
+
+/*
+ * class StreetIterator:
+ */
+StreetIterator::StreetIterator(DomainModel& domainModel)
+    : domainModel(domainModel) {}
+
+std::vector<Street>::const_iterator StreetIterator::cbegin() {
+  return domainModel.streets.cbegin();
+}
+
+std::vector<Street>::const_iterator StreetIterator::cend() {
+  return domainModel.streets.cend();
+}
+
+std::vector<Street>::iterator StreetIterator::begin() {
+  return domainModel.streets.begin();
+}
+
+std::vector<Street>::iterator StreetIterator::end() {
+  return domainModel.streets.end();
+}
+
+/*
+ * class JunctionIterator:
+ */
+JunctionIterator::JunctionIterator(DomainModel& domainModel)
+    : domainModel(domainModel) {}
+
+std::vector<Junction>::const_iterator JunctionIterator::cbegin() {
+  return domainModel.junctions.cbegin();
+}
+
+std::vector<Junction>::const_iterator JunctionIterator::cend() {
+  return domainModel.junctions.cend();
+}
+
+std::vector<Junction>::iterator JunctionIterator::begin() {
+  return domainModel.junctions.begin();
+}
+
+std::vector<Junction>::iterator JunctionIterator::end() {
+  return domainModel.junctions.end();
 }
--- source/domainmodel/DomainModel.h
+++ source/domainmodel/DomainModel.h
@@ -9,11 +9,12 @@
 
 class DomainModel {
 private:
-  std::vector<Vehicle> vehicles;
-  std::vector<Street> streets;
-  std::vector<Junction> junctions;
 
 public:
+  std::vector<Vehicle> vehicles; // TODO make private and use getter
+  std::vector<Street> streets; // TODO make private and use getter
+  std::vector<Junction> junctions; // TODO make private and use getter
+
   DomainModel() = default;
   Vehicle& addVehicle(Vehicle& vehicle);
   Street& addStreet(Street& street);
@@ -38,6 +39,28 @@ public:
   std::vector<Vehicle>::const_iterator cend();
   std::vector<Vehicle>::iterator begin();
   std::vector<Vehicle>::iterator end();
+};
+
+class StreetIterator {
+private:
+  DomainModel& domainModel;
+public:
+  StreetIterator(DomainModel& domainModel);
+  std::vector<Street>::const_iterator cbegin();
+  std::vector<Street>::const_iterator cend();
+  std::vector<Street>::iterator begin();
+  std::vector<Street>::iterator end();
+};
+
+class JunctionIterator {
+private:
+  DomainModel& domainModel;
+public:
+  JunctionIterator(DomainModel& domainModel);
+  std::vector<Junction>::const_iterator cbegin();
+  std::vector<Junction>::const_iterator cend();
+  std::vector<Junction>::iterator begin();
+  std::vector<Junction>::iterator end();
 };
 
 #endif
