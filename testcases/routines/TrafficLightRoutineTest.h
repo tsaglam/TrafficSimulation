fc40a4c9159c6f9a65359d8f92b7c9e204f4c454
Timur Saglam <uleay@student.kit.edu>
2019-01-12 11:55:53 +0100

    EDIT: Implemented stub for the integration test.

---
+++ testcases/routines/TrafficLightRoutineTest.h
@@ -0,0 +1,9 @@
+#include <../../snowhouse/snowhouse.h>
+
+void trafficLightRoutineTest() {
+	// TODO build domain model (TestFactory)
+	// TODO make instance of SimulationsData
+	// TODO initialize low level method (ModelSyncer)
+	// TODO call routine
+	// TODO check if traffic lights change
+}
