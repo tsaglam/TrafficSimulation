ddf18833fe73337b470e5283a7f9b779b36c72f4
Timur Saglam <uleay@student.kit.edu>
2019-01-17 17:27:10 +0100

    ADD: Added test for traffic light routine.

--- testcases/routines/TrafficLightRoutineTest.h
+++ testcases/routines/TrafficLightRoutineTest.h
@@ -1,9 +1,50 @@
+#include "../domainmodel/DomainModelTestFactory.h"
 #include <../../snowhouse/snowhouse.h>
 
+#include "ModelSyncer.h"
+#include "NaiveStreetDataStructure.h"
+#include "TrafficLightRoutine.h"
+#include <iostream>
+
+/**
+ *	Checks whether the traffic light routine works.
+ * The following Network is build:
+ *     x
+ *     |
+ *  x--x--x
+ *     |
+ *     x
+ */
 void trafficLightRoutineTest() {
-	// TODO build domain model (TestFactory)
-	// TODO make instance of SimulationsData
-	// TODO initialize low level method (ModelSyncer)
-	// TODO call routine
-	// TODO check if traffic lights change
+  // DOMAIN MODEL SETUP:
+  DomainModel model;
+  Junction &junction = model.addJunction(createTestJunction());
+  // clang-format off
+  for (CardinalDirection direction = CardinalDirection::NORTH; direction <= CardinalDirection::WEST;
+       direction = CardinalDirection(direction + 1)) { // clang-format on
+    CardinalDirection opposite = (CardinalDirection)((direction + 2) % 4);
+    Junction &other            = model.addJunction(Junction(0, 0, 10, 15, {{Junction::Signal(opposite, 40)}}));
+    Street &incoming           = model.addStreet(Street(direction, 1, 50.0, 100.0, other, junction));
+    Street &outgoing           = model.addStreet(Street(direction + 4, 1, 50.0, 100.0, junction, other));
+    junction.addIncomingStreet(incoming, direction);
+    junction.addOutgoingStreet(outgoing, direction);
+    other.addIncomingStreet(outgoing, opposite);
+    other.addOutgoingStreet(incoming, opposite);
+  }
+  // ROUTINE SETUP:
+  SimulationData<NaiveStreetDataStructure> data(model);
+  TrafficLightRoutine<NaiveStreetDataStructure> routine(data);
+  ModelSyncer<NaiveStreetDataStructure> modelSyncer(data);
+  modelSyncer.buildFreshLowLevel();
+  // ACTUAL TESTING:
+  AssertThat(junction.getCurrentSignal().getDirection(), Is().EqualTo(CardinalDirection::NORTH));
+  for (int i = 0; i <= 10; ++i) { routine.perform(); }
+  AssertThat(junction.getCurrentSignal().getDirection(), Is().EqualTo(CardinalDirection::EAST));
+  for (int i = 0; i <= 20; ++i) { routine.perform(); }
+  AssertThat(junction.getCurrentSignal().getDirection(), Is().EqualTo(CardinalDirection::SOUTH));
+  for (int i = 0; i <= 30; ++i) { routine.perform(); }
+  AssertThat(junction.getCurrentSignal().getDirection(), Is().EqualTo(CardinalDirection::WEST));
+  for (int i = 0; i <= 40; ++i) { routine.perform(); }
+  AssertThat(junction.getCurrentSignal().getDirection(), Is().EqualTo(CardinalDirection::NORTH));
+  // TODO check low level street
 }
