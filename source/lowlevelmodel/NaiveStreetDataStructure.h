88f4bf11009222f35d206785c03913b62c475955
fluegelk <katharina.fluegel@student.kit.edu>
2019-01-18 11:27:16 +0100

    Fix offset in NaiveStreetDataStructure::getNextCarBehind()

--- source/lowlevelmodel/NaiveStreetDataStructure.h
+++ source/lowlevelmodel/NaiveStreetDataStructure.h
@@ -148,22 +148,18 @@ public:
    * @return     The car behind the current car represented by an iterator.
    */
   iterator getNextCarBehind(const iterator currentCarIt, const int laneOffset = 0) {
-    // there is no car behind if this is the first car on the street
-    if (currentCarIt == carsOnStreet.begin()) { return carsOnStreet.end(); }
     const unsigned int lane = currentCarIt->getLane() + laneOffset;
     // iterate all cars behind the current car (on all lanes) return first car on the specified lane
-    for (iterator i = currentCarIt - 1; i != carsOnStreet.begin(); --i) { // reverse iteration
-      if (i->getLane() == lane) { return i; }
+    for (iterator i = currentCarIt; i != carsOnStreet.begin(); --i) { // reverse iteration
+      if ((i - 1)->getLane() == lane) { return (i - 1); }
     }
     return carsOnStreet.end(); // return end iterator if no next car exists on the given lane
   }
   const_iterator getNextCarBehind(const const_iterator currentCarIt, const int laneOffset = 0) const {
-    // there is no car behind if this is the first car on the street
-    if (currentCarIt == carsOnStreet.begin()) { return carsOnStreet.end(); }
     const unsigned int lane = currentCarIt->getLane() + laneOffset;
     // iterate all cars behind the current car (on all lanes) return first car on the specified lane
-    for (const_iterator i = currentCarIt - 1; i != carsOnStreet.begin(); --i) { // reverse iteration
-      if (i->getLane() == lane) { return i; }
+    for (const_iterator i = currentCarIt; i != carsOnStreet.begin(); --i) { // reverse iteration
+      if ((i - 1)->getLane() == lane) { return (i - 1); }
     }
     return carsOnStreet.end(); // return end iterator if no next car exists on the given lane
   }
