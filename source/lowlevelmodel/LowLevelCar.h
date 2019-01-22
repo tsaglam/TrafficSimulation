123dd9150758f0895f198e62f3fa82a1fdc22a1b
fluegelk <katharina.fluegel@student.kit.edu>
2019-01-22 12:58:13 +0100

    Store the traveled distance in each LowLevelCar, update it during the IDM routine

--- source/lowlevelmodel/LowLevelCar.h
+++ source/lowlevelmodel/LowLevelCar.h
@@ -54,6 +54,8 @@ private:
   double nextDistance;
   double nextVelocity;
 
+  double travelDistance = 0.0; // total distance traveled by this car in the current simulation
+
 public:
   LowLevelCar() = default;
   LowLevelCar(unsigned int _id, unsigned int _externalId, double _targetVelocity, double _maxAcceleration,
@@ -63,9 +65,10 @@ public:
         politeness(_politeness), length(_length) {}
   LowLevelCar(unsigned int _id, unsigned int _externalId, double _targetVelocity, double _maxAcceleration,
       double _accelerationDivisor, double _minDistance, double _targetHeadway, double _politeness, double _length,
-      unsigned int _lane, double _distance, double _velocity = 0.0)
+      unsigned int _lane, double _distance, double _velocity = 0.0, double _travelDistance = 0.0)
       : LowLevelCar(_id, _externalId, _targetVelocity, _maxAcceleration, _accelerationDivisor, _minDistance,
             _targetHeadway, _politeness, _length) {
+    travelDistance = _travelDistance;
     setPosition(_lane, _distance, _velocity);
     setNext(_lane, _distance, _velocity);
   }
@@ -123,6 +126,10 @@ public:
     nextDistance = distance;
     nextVelocity = velocity;
   }
+
+  // Interface for measuring the traveled distance per car for the traffic light optimization
+  void updateTravelDistance(const double additionalDistance) { travelDistance += additionalDistance; }
+  double getTravelDistance() const { return travelDistance; }
 };
 
 #endif
