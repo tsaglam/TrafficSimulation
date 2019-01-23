5621735cc912f67213097791ade55d7e9d28c602
fluegelk <katharina.fluegel@student.kit.edu>
2019-01-23 13:43:02 +0100

    Overload OptimizationRoutine::determinePotentialTravelDistance to switch between different algorithms based on the RfbIterator implementation

--- source/routines/OptimizationRoutine.h
+++ source/routines/OptimizationRoutine.h
@@ -6,17 +6,25 @@
 #include "DomainModel.h"
 #include "DomainModelCommon.h"
 #include "Junction.h"
+#include "RfbStructureTraits.h"
 #include "SimulationData.h"
 
 template <template <typename Vehicle> typename RfbStructure>
 class OptimizationRoutine {
+  using reverse_category = typename rfbstructure_traits<RfbStructure>::reverse_category;
 
 private:
   SimulationData<RfbStructure> &data;
   // contains for each junction a vector of requested green light directions per time step
   std::vector<std::vector<CardinalDirection>> requestedGreenLights;
 
-  double determinePotentialTravelDistance(const LowLevelStreet<RfbStructure>& street) const {
+  double determinePotentialTravelDistance(
+      const LowLevelStreet<RfbStructure> &street, rfbstructure_reversible_sorted_iterator_tag) const {
+    // TODO implement this
+    return street.getLength();
+  }
+
+  double determinePotentialTravelDistance(const LowLevelStreet<RfbStructure> &street, rfbstructure_buckets_tag) const {
     // TODO implement this
     return street.getLength();
   }
@@ -24,12 +32,12 @@ private:
   CardinalDirection determineOptimalGreenLight(const Junction &junction) const {
     double maxPotentialTravelDistance = -1.0;
     CardinalDirection requestedDirection;
-    for (const auto& incomingStreet : junction.getIncomingStreets()) {
-      const auto& lowLevelStreet = data.getStreet(incomingStreet.getStreet()->getId());
-      double potentialTravelDistance = determinePotentialTravelDistance(lowLevelStreet);
+    for (const auto &incomingStreet : junction.getIncomingStreets()) {
+      const auto &lowLevelStreet     = data.getStreet(incomingStreet.getStreet()->getId());
+      double potentialTravelDistance = determinePotentialTravelDistance(lowLevelStreet, reverse_category());
       if (potentialTravelDistance > maxPotentialTravelDistance) {
         maxPotentialTravelDistance = potentialTravelDistance;
-        requestedDirection = incomingStreet.getDirection();
+        requestedDirection         = incomingStreet.getDirection();
       }
     }
     return requestedDirection;
