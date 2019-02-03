b1d1d68cdb9eb66e154d6da98b86a0020fb6d454
fluegelk <katharina.fluegel@student.kit.edu>
2019-02-03 16:25:33 +0100

    Fix bench-all.sh and add actual input directories
    Former-commit-id: e183022909ef9dd8b04d94e73d86171a6c4043cc [formerly 29cf26e981a1591b609305d0dc0920d4e20d1edb]
    Former-commit-id: c45721048ce16f4cdfa3b928a5e34ed2328beda3

--- tools/bench-all.sh
+++ tools/bench-all.sh
@@ -4,13 +4,17 @@ traffic_sim_variant=$1 # description of the currently compiled traffic sim (i.e.
 output_file="bench-all-results.csv"
 
 function runBenchmark {
-	carAmount=$1
+	dimension=$1
 	inputPath=$2
-	prefix="\"${traffic_sim_variant}, ${carAmount}\""
-	eval "python3 bench.py --no-header --row-prefix ${prefix} ${inputPath} >> ${output_file}"
+	prefix="\"${traffic_sim_variant}, ${dimension}\""
+	eval "python3 bench.py --no-header --row-prefix ${prefix} ../traffic_sim ${inputPath} >> ${output_file}"
 }
 
 eval "python3 bench.py --header-only --header-prefix \"traffic_sim_variant, dimension\" ir relevant > ${output_file}"
 
 # hardcoded for all input paths
-# runBenchmark <carAmount> <inputPath>
+# runBenchmark <dimension> <inputPath>
+runBenchmark "carAmount" "generated_tests/carAmount"
+runBenchmark "gridSize" "generated_tests/gridSize"
+runBenchmark "sparseScaling" "generated_tests/sparseScaling"
+runBenchmark "streetLength" "generated_tests/streetLength"
